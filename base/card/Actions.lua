eae494da9802abc3a3092bb554d8e6cc6846548d
lkstc112233 <296105011@qq.com>
2017-03-06 00:27:09 +0800

    1st version
    Add elements.
    
    Should make a method for data transfering.

---
+++ base/card/Actions.lua
@@ -0,0 +1,3 @@
+function singleAttack(CounterType)
+
+end
---
+++ base/card/Elements/Fire_Base_Element.lua
@@ -0,0 +1,10 @@
+data:extend(
+{
+	{
+		type = "Element",
+		name = "Fire_Element"
+		valid_targets_type = "Player",
+		action = singleAttack("Water_Element")
+	},
+}
+)
